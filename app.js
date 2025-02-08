f833767165a9f382e84d4b207c12bbe6c904ffd7
sastanoobie <sastanoobie2@gmail.com>
2025-02-08 20:42:26 +0530

    new changes

--- app.js
+++ app.js
@@ -10,31 +10,48 @@ let city = document.getElementById(`city`);
 let humi = document.getElementById(`humi`);
 let icon = document.querySelector(`.theme img`);
 
+document.getElementById("root").style.display = "none"
 let get = async (place)=>{
-    let response = await fetch(url + place + `&appid=${APIkey}`);
-    let data = await response.json();
-    console.log(data);
-    temp.innerHTML = Math.floor( data.main.temp)+"°c";
-    wind.innerText = Math.floor(data.wind.speed)+"Km/h";
-    des.innerHTML = data.weather[0].main;
-    city.innerHTML = data.name;
-    humi.innerHTML = data.main.humidity;
+    document.querySelector("section").style.display = "none"
+    document.getElementById("root").style.display = "flex"
 
-   if(data.weather[0].main == "Clouds"){
-    icon.src = "images/clouds.png"
-   }else if(data.weather[0].main == "Clear"){
-      icon.src = "images/clear.png"
-   }else if(data.weather[0].main == "Rain"){
-    icon.src = "images/rain.png"
-   }else if(data.weather[0].main == "Drizzle"){
-    icon.src = "images/drizzle.png"
-   }else if(data.weather[0].main == "Mist"){
-    icon.src = "images/mist.png"
-   }else if(data.weather[0].main == "Snow"){
-    icon.src = "images/snow.png"
-   }else{
-     icon.src = "images/clouds.png"
-   }
+try{
+  let response = await fetch(url + place + `&appid=${APIkey}`);
+  let data = await response.json();
+  console.log(data);
+  temp.innerHTML = Math.floor( data.main.temp)+"°c";
+  wind.innerText = Math.floor(data.wind.speed)+"Km/h";
+  des.innerHTML = data.weather[0].main;
+  city.innerHTML = data.name;
+  humi.innerHTML = data.main.humidity;
+
+ if(data.weather[0].main == "Clouds"){
+  icon.src = "images/clouds.png"
+ }else if(data.weather[0].main == "Clear"){
+    icon.src = "images/clear.png"
+ }else if(data.weather[0].main == "Rain"){
+  icon.src = "images/rain.png"
+ }else if(data.weather[0].main == "Drizzle"){
+  icon.src = "images/drizzle.png"
+ }else if(data.weather[0].main == "Mist"){
+  icon.src = "images/mist.png"
+ }else if(data.weather[0].main == "Snow"){
+  icon.src = "images/snow.png"
+ }else{
+   icon.src = "images/clouds.png"
+ }
+
+  document.querySelector("section").style.display = "block"
+
+}catch(error){
+
+  alert("error")
+
+}finally{
+    document.getElementById("root").style.display = "none"
+}
+    
+
 }
 
 btn.addEventListener("click",()=>{
